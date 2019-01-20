eda3060fe8e73e8350c65b7071a972c4d3244590
fredx100 <26229723+fredx100@users.noreply.github.com>
2019-01-20 23:09:57 +0000

    Adding recyclerView adaptor for ListItem.

--- app/src/main/java/uk/sensoryunderload/infinilist/ListItem.java
+++ app/src/main/java/uk/sensoryunderload/infinilist/ListItem.java
@@ -1,15 +1,26 @@
 package uk.sensoryunderload.infinilist;
 
+// Possible status values for a ListItem
+enum Status {
+    NONE,
+    SUCCESS,
+    FAIL,
+    IMPORTANT
+}
+
 // StatusFlag
 // Records a status from
-//     none
-//     success
-//     fail
-//     important
+//     none (0)
+//     success (1)
+//     fail (2)
+//     important (3)
 class StatusFlag {
     private int flag;
 
-    public void cycle() { flag = (flag + 1) % 4; }
+    StatusFlag() {
+        flag = 0;
+    }
+    void cycle() { flag = (flag + 1) % 4; }
 }
 
 class ListItem {
@@ -17,6 +28,20 @@ class ListItem {
     private String content;
     private StatusFlag status;
     private java.util.List<ListItem> children;
+
+    public ListItem (String _title, String _content, StatusFlag _sFlag) {
+        title = _title;
+        content = _content;
+        status = _sFlag;
+        children = new java.util.ArrayList<ListItem>();
+    }
+
+    public ListItem (String _title, String _content) {
+        title = _title;
+        content = _content;
+        status = new StatusFlag();
+        children = new java.util.ArrayList<ListItem>();
+    }
 
     public String getTitle() {
         return title;
@@ -30,7 +55,9 @@ class ListItem {
         return status;
     }
 
-    public changeStatus() {
+    public java.util.List<ListItem> getChildren() { return children; }
+
+    public void changeStatus() {
         status.cycle();
     }
 }
---
+++ app/src/main/java/uk/sensoryunderload/infinilist/ListItemAdaptor.java
@@ -0,0 +1,59 @@
+package uk.sensoryunderload.infinilist;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+
+import java.util.List;
+
+final class ListItemAdapter extends RecyclerView.Adapter<ListItemAdapter.MyViewHolder> {
+
+    private List<ListItem> itemList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title, content;
+        public CheckBox flag;
+        public ImageView hasChildrenImage;
+
+        public MyViewHolder(View view) {
+            super(view);
+            title = (TextView) view.findViewById(R.id.title);
+            content = (TextView) view.findViewById(R.id.content);
+            flag = (CheckBox) view.findViewById(R.id.rowStatus);
+            hasChildrenImage = (ImageView) view.findViewById(R.id.rowHasChildren);
+        }
+    }
+
+    public ListItemAdapter(List<ListItem> itemList) {
+        this.itemList = itemList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item_row, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        ListItem item = itemList.get(position);
+        holder.title.setText(item.getTitle());
+        holder.content.setText(item.getContent());
+        // TODO: Populate status flag
+        // TODO: Optionally hide hasChildren image:
+//        if (item.hasChildren()) {
+//            holder.hasChildrenImage.src = "";
+//        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+}
