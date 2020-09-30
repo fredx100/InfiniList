80dc1a2d9f11fb1fa324801be81f4f22d05ce477
fredx100 <26229723+fredx100@users.noreply.github.com>
2020-09-30 15:55:17 +0100

    WIP: partial widget files.

---
+++ app/src/main/java/uk/sensoryunderload/infinilist/ListWidgetProvider.java
@@ -0,0 +1,8 @@
+public class ListWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate (Context context,
+                          AppWidgetManager appWidgetManager,
+                          int[] appWidgetIds) {
+        // TODO
+    }
+}
